a4f55e83dedac42a1a07ec07b0f187495c71e2a0
nukotaiga <Miyavich129@gmail.com>
2024-07-09 14:25:07 +0900

    変更しました

--- script.js
+++ script.js
@@ -1,100 +1,123 @@
-const A = document.querySelector('#A')
+// const A = document.querySelector('#A')
+// const B = document.querySelector('#B')
+// const C = document.querySelector('#C')
+
+// let count = 0
+
+// A.addEventListener('click', () => {
+//     count++;
+//     // 奇数偶数の判定
+//     if (count%2===1){
+//         A.textContent = '〇'
+//     } else {
+//         A.textContent = '×'
+//     }
+// })
+
+// B.addEventListener('click', () => {
+//     count++;
+//     // 奇数偶数の判定
+//     if (count%2===1){
+//         B.textContent = '〇'
+//     } else {
+//         B.textContent = '×'
+//     }
+// })
+
+// C.addEventListener('click', () => {
+//     count++;
+//     // 奇数偶数の判定
+//     if (count%2===1){
+//         C.textContent = '〇'
+//     } else {
+//         C.textContent = '×'
+//     }
+// })
+
+// これは配列が入る
+const masu = document.querySelectorAll('.board div');
+console.log(masu);
+
 let count = 0
 
-A.addEventListener('click', () => {
+masu[0].addEventListener('click', () => {
     count++;
-    //奇数偶数の判定
-    if (count%2===1){
-        A.textContent = '〇'
+    // 奇数偶数の判定
+    if (count % 2 === 1) {
+        masu[0].textContent = '〇'
     } else {
-        A.textContent = '×'
+        masu[0].textContent = '×'
     }
 })
-const B = document.querySelector('#B')
-
-B.addEventListener('click', () => {
+masu[1].addEventListener('click', () => {
     count++;
-    //奇数偶数の判定
-    if (count%2===1){
-        B.textContent = '〇'
+    // 奇数偶数の判定
+    if (count % 2 === 1) {
+        masu[1].textContent = '〇'
     } else {
-        B.textContent = '×'
+        masu[1].textContent = '×'
     }
 })
-const C = document.querySelector('#C')
-
-C.addEventListener('click', () => {
+masu[2].addEventListener('click', () => {
     count++;
-    //奇数偶数の判定
-    if (count%2===1){
-        C.textContent = '〇'
+    // 奇数偶数の判定
+    if (count % 2 === 1) {
+        masu[2].textContent = '〇'
     } else {
-        C.textContent = '×'
+        masu[2].textContent = '×'
     }
 })
-const D = document.querySelector('#D')
-
-D.addEventListener('click', () => {
+masu[3].addEventListener('click', () => {
     count++;
-    //奇数偶数の判定
-    if (count%2===1){
-        D.textContent = '〇'
+    // 奇数偶数の判定
+    if (count % 2 === 1) {
+        masu[3].textContent = '〇'
     } else {
-        D.textContent = '×'
+        masu[3].textContent = '×'
     }
 })
-const E = document.querySelector('#E')
-
-E.addEventListener('click', () => {
+masu[4].addEventListener('click', () => {
     count++;
-    //奇数偶数の判定
-    if (count%2===1){
-        E.textContent = '〇'
+    // 奇数偶数の判定
+    if (count % 2 === 1) {
+        masu[4].textContent = '〇'
     } else {
-        E.textContent = '×'
+        masu[4].textContent = '×'
     }
 })
-const F = document.querySelector('#F')
-
-F.addEventListener('click', () => {
+masu[5].addEventListener('click', () => {
     count++;
-    //奇数偶数の判定
-    if (count%2===1){
-        F.textContent = '〇'
+    // 奇数偶数の判定
+    if (count % 2 === 1) {
+        masu[5].textContent = '〇'
     } else {
-        F.textContent = '×'
+        masu[5].textContent = '×'
     }
 })
-const G = document.querySelector('#G')
-
-G.addEventListener('click', () => {
+masu[6].addEventListener('click', () => {
     count++;
-    //奇数偶数の判定
-    if (count%2===1){
-        G.textContent = '〇'
+    // 奇数偶数の判定
+    if (count % 2 === 1) {
+        masu[6].textContent = '〇'
     } else {
-        G.textContent = '×'
+        masu[6].textContent = '×'
     }
 })
-const H = document.querySelector('#H')
-
-H.addEventListener('click', () => {
+masu[7].addEventListener('click', () => {
     count++;
-    //奇数偶数の判定
-    if (count%2===1){
-        H.textContent = '〇'
+    // 奇数偶数の判定
+    if (count % 2 === 1) {
+        masu[7].textContent = '〇'
     } else {
-        H.textContent = '×'
+        masu[7].textContent = '×'
     }
 })
-const I = document.querySelector('#I')
-
-I.addEventListener('click', () => {
+masu[8].addEventListener('click', () => {
     count++;
-    //奇数偶数の判定
-    if (count%2===1){
-        I.textContent = '〇'
+    // 奇数偶数の判定
+    if (count % 2 === 1) {
+        masu[8].textContent = '〇'
     } else {
-        I.textContent = '×'
+        masu[8].textContent = '×'
     }
 })
