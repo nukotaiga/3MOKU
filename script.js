802070725ee6994c5d552caa105aef76e0d4df3c
nukotaiga <Miyavich129@gmail.com>
2024-07-09 13:59:22 +0900

    変更

---
+++ script.js
@@ -0,0 +1,100 @@
+const A = document.querySelector('#A')
+let count = 0
+
+A.addEventListener('click', () => {
+    count++;
+    //奇数偶数の判定
+    if (count%2===1){
+        A.textContent = '〇'
+    } else {
+        A.textContent = '×'
+    }
+})
+const B = document.querySelector('#B')
+
+B.addEventListener('click', () => {
+    count++;
+    //奇数偶数の判定
+    if (count%2===1){
+        B.textContent = '〇'
+    } else {
+        B.textContent = '×'
+    }
+})
+const C = document.querySelector('#C')
+
+C.addEventListener('click', () => {
+    count++;
+    //奇数偶数の判定
+    if (count%2===1){
+        C.textContent = '〇'
+    } else {
+        C.textContent = '×'
+    }
+})
+const D = document.querySelector('#D')
+
+D.addEventListener('click', () => {
+    count++;
+    //奇数偶数の判定
+    if (count%2===1){
+        D.textContent = '〇'
+    } else {
+        D.textContent = '×'
+    }
+})
+const E = document.querySelector('#E')
+
+E.addEventListener('click', () => {
+    count++;
+    //奇数偶数の判定
+    if (count%2===1){
+        E.textContent = '〇'
+    } else {
+        E.textContent = '×'
+    }
+})
+const F = document.querySelector('#F')
+
+F.addEventListener('click', () => {
+    count++;
+    //奇数偶数の判定
+    if (count%2===1){
+        F.textContent = '〇'
+    } else {
+        F.textContent = '×'
+    }
+})
+const G = document.querySelector('#G')
+
+G.addEventListener('click', () => {
+    count++;
+    //奇数偶数の判定
+    if (count%2===1){
+        G.textContent = '〇'
+    } else {
+        G.textContent = '×'
+    }
+})
+const H = document.querySelector('#H')
+
+H.addEventListener('click', () => {
+    count++;
+    //奇数偶数の判定
+    if (count%2===1){
+        H.textContent = '〇'
+    } else {
+        H.textContent = '×'
+    }
+})
+const I = document.querySelector('#I')
+
+I.addEventListener('click', () => {
+    count++;
+    //奇数偶数の判定
+    if (count%2===1){
+        I.textContent = '〇'
+    } else {
+        I.textContent = '×'
+    }
+})
